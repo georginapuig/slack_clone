38f1395a521fe85916700f696d0828904cf8e133
georgina puig <georginaxcodes@gmail.com>
2020-07-01 12:31:06 -0500

    import initChatroomCable

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -24,12 +24,12 @@ require("channels");
 
 // External imports
 import "bootstrap";
-import { initChatroomCannel } from '../channel/chatrooms_channel';
+import { initChatroomCable } from '../channels/chatroom_channel';
 // Internal imports, e.g:
 // import { initSelect2 } from '../components/init_select2';
 
 document.addEventListener('turbolinks:load', () => {
   // Call your functions here, e.g:
   // initSelect2();
-  initChatroomCannel();
+  initChatroomCable();
 });
