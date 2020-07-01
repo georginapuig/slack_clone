30d051b419869db68fbbbc6b38b6653de7a5a168
georgina puig <georginaxcodes@gmail.com>
2020-07-01 12:08:24 -0500

    imported initChatroomCannel

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -3,10 +3,10 @@
 // a relevant structure within app/javascript and only use these pack files to reference
 // that code so it'll be compiled.
 
-require("@rails/ujs").start()
-require("turbolinks").start()
-require("@rails/activestorage").start()
-require("channels")
+require("@rails/ujs").start();
+require("turbolinks").start();
+require("@rails/activestorage").start();
+require("channels");
 
 
 // Uncomment to copy all static images under ../images to the output folder and reference
@@ -24,11 +24,12 @@ require("channels")
 
 // External imports
 import "bootstrap";
-
+import { initChatroomCannel } from '../channel/chatrooms_channel';
 // Internal imports, e.g:
 // import { initSelect2 } from '../components/init_select2';
 
 document.addEventListener('turbolinks:load', () => {
   // Call your functions here, e.g:
   // initSelect2();
+  initChatroomCannel();
 });
