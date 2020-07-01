8ca9676a8af1e582a9278b96f6e190eac80f57e4
georgina puig <georginaxcodes@gmail.com>
2020-07-01 11:52:52 -0500

    subscribed

--- app/channels/chatroom_channel.rb
+++ app/channels/chatroom_channel.rb
@@ -1,6 +1,8 @@
 class ChatroomChannel < ApplicationCable::Channel
   def subscribed
     # stream_from "some_channel"
+    chatroom = Chatroom.find(params[:id])
+    stream_for chatroom
   end
 
   def unsubscribed
